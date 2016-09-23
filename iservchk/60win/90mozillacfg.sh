191da7d4303045a35399c1cdf7209d8093873a9d
Felix Jacobi <felix.jacobi@stsbl.de>
2016-09-23 03:03:14 +0200

    Fixed iconf scripts. #2

--- iservchk/60win/90mozillacfg.sh
+++ iservchk/60win/90mozillacfg.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-if [ $(dpkg-query -W --showformat='${Status}\n' winst-mozilla-firefox | grep "install ok installed") ]
+if [[ $(dpkg-query --showformat='${Status}\n' --show "winst-mozilla-firefox" | grep "install ok installed") ]]
 then  
   echo 'Check "/srv/deploy/install/mozilla-firefox/add/mozilla.cfg"'
   echo
   echo '# re-install Firefox to all Computers which has Firefox installed when the mozilla.cfg has been modified by the check'
   echo
-  echo 'Shell "Re-install Mozilla Firefox on all computers"'
-  echo "! [ $REPAIR ] || psql -c "UPDATE deploy_state SET action='setup'"
-  echo '  WHERE product = 'mozilla-firefox';" iserv postgres'
+  echo "Shell \"Re-install Mozilla Firefox on all computers\" ! [ \$REPAIR ] || psql -c \"UPDATE deploy_state SET action='setup' WHERE product = 'mozilla-firefox';\" iserv postgres"
   echo
-  echo 'Shell "Remove backup files of mozilla.cfg"'
-  echo '! [ $REPAIR ] || rm -f /srv/deploy/install/mozilla-firefox/add/mozilla.cfg.*'
+  echo 'Shell "Remove backup files of mozilla.cfg" ! [ \$REPAIR ] || rm -f /srv/deploy/install/mozilla-firefox/add/mozilla.cfg.*'
 fi
