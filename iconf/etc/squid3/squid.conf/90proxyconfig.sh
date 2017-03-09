fbb7d65330fc97adb6705779b04daa311525cccb
Felix Jacobi <felix.jacobi@stsbl.de>
2017-03-09 22:45:30 +0100

    Improved syntax of squid config.

--- iconf/etc/squid3/squid.conf/90proxyconfig.sh
+++ iconf/etc/squid3/squid.conf/90proxyconfig.sh
@@ -2,14 +2,14 @@
 
 . /usr/lib/iserv/cfg
 
-if [ "$TrustedChildProxies" != "" ]
+if [ ! -z "$TrustedChildProxies"  ]
 then
   echo "# Read X-Forwarded-For from Child Proxy"
   for TrustedChildProxy in "${TrustedChildProxies[@]}"
   do
-    echo "acl child_proxy src $TrustedChildProxy"
+    echo "  acl child_proxy src $TrustedChildProxy"
   done 
   echo
-  echo "follow_x_forwarded_for allow child_proxy"
+  echo "  follow_x_forwarded_for allow child_proxy"
   echo
 fi
