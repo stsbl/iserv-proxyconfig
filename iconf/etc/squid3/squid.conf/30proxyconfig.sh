45871628d0bb11227033a9edab752c4b588221ea
Felix Jacobi <felix.jacobi@stsbl.de>
2017-03-12 00:54:56 +0100

    Squid config improvements:
    Redirect Windows updates to the Windows update proxy instead of a configured peer.
    Include subdomains for domains excluded from the cache peer.

--- iconf/etc/squid3/squid.conf/30proxyconfig.sh
+++ iconf/etc/squid3/squid.conf/30proxyconfig.sh
@@ -13,7 +13,13 @@ if [ ! -z "$ProxyCachePeer" ]
 then
   PEERSTRING=$(echo $ProxyCachePeer | sed 's/:/ parent /g')
   echo "# Cache peer"
-  echo "  cache_peer $PEERSTRING 0 default never-direct no-query no-digest no-netdb-exchange"
+  echo "  cache_peer $PEERSTRING 0 default name=peer never-direct no-query no-digest no-netdb-exchange"
+  echo
+  echo "# domains which should not use the upstream proxy"
+  echo "  acl peer_black_list dstdomain .windowsupdate.com"
+  echo
+  echo "# Send windows update requests still via nginx"
+  echo "  cache_peer_access peer allow !peer_black_list"
   echo
   echo "# Allow redirection of CONNECT requests to the cache peer"
   echo "  never_direct allow CONNECT"
@@ -21,13 +27,14 @@ then
   if [ ! -z "$ProxyAlwaysDirect" ]
   then
     echo "# Domains which should bypass the cache peer"
-    for i in "$ProxyAlwaysDirect"
+    for i in "${ProxyAlwaysDirect[@]}"
     do
-      echo "  acl cache_peer_bypass dstdomain $i"
+      echo "  acl cache_peer_bypass dstdomain .$i"
     done
     echo
     echo "# Bypass the upstream proxy for the selected domains"
     echo "  always_direct allow cache_peer_bypass"
+    echo "  cache_peer_access peer deny cache_peer_bypass"
     echo
   fi
 fi
