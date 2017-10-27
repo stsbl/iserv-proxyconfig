2763af7ae7de8f17f8ad18dc1bc39878f0aaf506
Felix Jacobi <felix.jacobi@iserv.eu>
2017-10-28 00:26:57 +0200

    Adapted module for Debian Stretch

---
+++ iconf/etc/squid/squid.conf/30proxyconfig.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+. /usr/lib/iserv/cfg
+
+if [ ! -z "$ProxyCacheMgr" ]
+then
+  echo "# Cache Administrator"
+  echo "  cache_mgr $ProxyCacheMgr"
+  echo ""
+fi
+
+if [ ! -z "$ProxyCachePeer" ]
+then
+  PEERSTRING=$(echo $ProxyCachePeer | sed 's/:/ parent /g')
+  echo "# Cache peer"
+  echo "  cache_peer $PEERSTRING 0 default name=peer never-direct no-query no-digest no-netdb-exchange"
+  echo
+  echo "# domains which should not use the upstream proxy"
+  echo "  acl peer_black_list dstdomain .windowsupdate.com"
+  echo
+  echo "# Send windows update requests still via nginx"
+  echo "  cache_peer_access peer deny peer_black_list"
+  echo "  cache_peer_access peer allow all !peer_black_list"
+  echo
+  echo "# Allow redirection of CONNECT requests to the cache peer"
+  echo "  never_direct allow CONNECT"
+  echo
+  if [ ! -z "$ProxyAlwaysDirect" ]
+  then
+    echo "# Domains which should bypass the cache peer"
+    for i in "${ProxyAlwaysDirect[@]}"
+    do
+      echo "  acl cache_peer_bypass dstdomain .$i"
+    done
+    echo
+    echo "# Bypass the upstream proxy for the selected domains"
+    echo "  always_direct allow cache_peer_bypass"
+    echo "  cache_peer_access peer deny cache_peer_bypass"
+    echo
+  fi
+fi
--- iconf/etc/squid3/squid.conf/90proxyconfig.sh
+++ iconf/etc/squid3/squid.conf/90proxyconfig.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
-
-. /usr/lib/iserv/cfg
-
-if [ ! -z "$TrustedChildProxies"  ]
-then
-  echo "# Read X-Forwarded-For from Child Proxy"
-  for TrustedChildProxy in "${TrustedChildProxies[@]}"
-  do
-    echo "  acl child_proxy src $TrustedChildProxy"
-  done 
-  echo
-  echo "  follow_x_forwarded_for allow child_proxy"
-  echo
-fi
+../../squid/squid.conf/90proxyconfig.sh
